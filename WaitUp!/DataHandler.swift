a24489f3e6904a22082c81190cee18b792c7fe90
Max Zuo <maxzuo@maxs-macbook-pro.local>
2016-10-08 11:49:33 -0400

    2

---
+++ WaitUp!/DataHandler.swift
@@ -0,0 +1,24 @@
+//
+//  DataHandler.swift
+//  WaitUp!
+//
+//  Created by Max Zuo on 10/8/16.
+//  Copyright © 2016 liZUOrd. All rights reserved.
+//
+
+import UIKit
+
+class DataHandler {
+    
+    private func retrieveBusinfo() {
+        
+    }
+    
+    class func retrieveSchoolNames() -> Array<String>? { // Remove optional once actually receiving data
+        
+        return nil
+    }
+    
+    
+    
+}
---
+++ WaitUp!/MapViewController.swift
@@ -0,0 +1,24 @@
+//
+//  MapViewController.swift
+//  WaitUp!
+//
+//  Created by Max Zuo on 10/8/16.
+//  Copyright © 2016 liZUOrd. All rights reserved.
+//
+
+import UIKit
+
+class MapViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
