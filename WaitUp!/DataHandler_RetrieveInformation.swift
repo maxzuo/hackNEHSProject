a69fdacb16b6aa70c7053b83d61056f443747934
Max Zuo <maxzuo@maxs-macbook-pro.local>
2016-10-08 19:45:26 -0400

    Fixed DataHandler Receiver

--- WaitUp!/DataHandler_RetrieveInformation.swift
+++ WaitUp!/DataHandler_RetrieveInformation.swift
@@ -12,22 +12,57 @@ import FirebaseDatabase
 
 extension DataHandler {
     
-    class func retrieveBusinfo() {
+    class func retrieveBusinfo() -> [Float]? {
         
-    }
-    
-    class func retrieveSchoolNames() -> Array<String>? { // Remove optional once actually receiving data
-        return nil
-    }
-    
-    class func retrieveSchoolInfo() -> String? {
         let defaults = UserDefaults.standard
-        if let selectedSchool = defaults.value(forKey: "School") as? NSString {
-            // retrieve post with name selectedSchool
-            // return info as string
+        let bus = defaults.value(forKey: "Bus") as? NSString as! String
+        
+        let text = DataHandler.retrieveSchoolInfo()?.components(separatedBy: "\n")
+        
+        for line in text! {
+            if line.contains(bus) {
+                let information = line.components(separatedBy: "\t")
+                return [(NumberFormatter().number(from: information[1])?.floatValue)!, (NumberFormatter().number(from: information[2])?.floatValue)!]
+            }
         }
         
         return nil
+    }
+    
+    class func retrieveSchoolNames() -> Array<String> { // Remove optional once actually receiving data
+
+        var schoolNames: Array<String> = []
+        let database = FIRDatabase.database().reference()
+        
+        database.child("School_Names").queryOrderedByKey().observe(.childChanged, with: {
+            snapshot in
+            schoolNames = (snapshot.value! as! String).components(separatedBy: "\t")
+            
+        })
+        
+        return schoolNames
         
     }
+    class func retrieveSchoolInfo() -> String? {
+        
+        var schoolName: String = ""
+        
+        let defaults = UserDefaults.standard
+        
+        if let selectedSchool = defaults.value(forKey: "School") as? NSString {
+            // retrieve post with name selectedSchool
+            // return info as string
+            let database = FIRDatabase.database().reference()
+            database.child(selectedSchool as! String).queryOrderedByKey().observe(.childChanged, with: {
+                snapshot in
+                schoolName = snapshot.value! as! String
+                
+            })
+            
+            return schoolName
+            
+        }
+        return nil
+    }
+    
 }
